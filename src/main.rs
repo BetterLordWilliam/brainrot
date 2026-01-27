5130d32145baaa962b24d3b968c04143e5f9e53a
BetterLordWilliam <willjordan05@icloud.com>
2026-01-27 13:32:54 -0800

    WO: basic application created for brainrot.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,56 @@
 use std::io;
 use ratatui::{
-    DefaultTerminal, buffer::Buffer, layout::Rect
+    DefaultTerminal, Frame, crossterm::event::{self, Event, KeyCode, KeyEvent}, layout::{ Alignment, Rect }, style::Stylize, symbols::border, text::Line, widgets::{ Block, Borders, Paragraph }
 };
+
 
 #[derive(Default)]
 struct App {
     quit: bool
 }
 
-// This is a so-called implementation
-// This has methods of a struct, among other things
+
 impl App {
     fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.quit {
+            if let Event::Key(key) = event::read()? {
+                if key.kind == event::KeyEventKind::Press {
+                    self.handle_key_press(key);
+                }
+            }
+
             terminal.draw(|frame| {
-                let block = Block::new();
-                frame.render_widget(widget, area);
-            });
+                let b = Block::bordered()
+                    .title(Line::raw("brainrot").bold())
+                    .title_bottom(Line::default().spans(vec![
+                        "quit ".blue(),
+                        "<q>".blue().bold()
+                    ]))
+                    .title_alignment(Alignment::Center);
+                let p = Paragraph::new("welcome to brainrot")
+                    .block(b);
+
+                frame.render_widget(p, frame.area());
+            })?;
+
         };
         Ok(())
     }
 
-    fn render(&mut self, area: Rect, buf: &mut Buffer) {
+    fn render(&self, frame: &mut Frame) {
 
     }
+
+    fn handle_key_press(&mut self, key: KeyEvent) {
+        match key.code {
+            KeyCode::Char('q') => {
+                self.quit = true;
+            },
+            _ => {}
+        };
+    }
 }
+
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
