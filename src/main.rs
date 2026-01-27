0a14a25486d1297c3786daa1d75fb52e29451cf3
BetterLordWilliam <willjordan05@icloud.com>
2026-01-27 12:09:18 -0800

    WO: created a terminal application that will switch to an alternative buffer and then lock control of the world.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use ratatui::{
+    DefaultTerminal, Frame
+};
+
+fn app_loop(terminal: &mut DefaultTerminal) -> io::Result<()> {
+    loop {
+        // App code for the main loop begins in this place
+    };
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let res = app_loop(&mut terminal);
+    ratatui::restore();
+
+    res
 }
